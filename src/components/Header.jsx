c3b57b5d74762982ac433dfb4d2e6279924b56c1
Katie Raby <katie.raby@icloud.com>
2020-02-20 17:33:48 +0000

    Additional styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function Header(props) {
   return (
     <header className="header">
-      <h1 className="header__h1">Quakr</h1>
+      <h1 className="header__h1">QUAKR</h1>
+      <p className="header__p">
+        An up-to-date visualisation of earthquakes across the world
+      </p>
     </header>
   );
 }
